dbebd9e7ad5b8b778e2e2feae71ac1e94fa17f43
victorvergara1784 <vitola1784@hotmail.com>
2016-03-01 20:00:17 -0500

    Mostrar
    Mostrar Compilación Parcial

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
